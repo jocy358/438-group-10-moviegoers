42b52a46028f6345b20e03ae85a0d6ce5f29c5e0
Jacob Zamora <113962665+JZmor@users.noreply.github.com>
2025-09-03 07:06:24 -0700

    Made file for profile page screen. No changes to packages they just swapped around

---
+++ Moviegoers/app/profile_screen.tsx
@@ -0,0 +1,17 @@
+//Background color changed to show if working
+import { Text, View } from "react-native";
+
+export default function ProfileScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "#DFC5FE",
+        alignItems: "center",
+      }}
+    >
+      <Text>Profile</Text>
+    </View>
+  );
+}
