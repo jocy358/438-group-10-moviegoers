89d699eceb347e52c547e612925f604c50b5ffce
Serafiend7 <eperegoy@csumb.edu>
2025-09-09 12:52:05 -0700

    Got user by username
    Changed index back to go to DbTest
    Added code to display the user information after getting it by username

--- Moviegoers/app/dbTest.tsx
+++ Moviegoers/app/dbTest.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-import { Button, View, StyleSheet } from "react-native";
+import React, {useState} from "react";
+import { Text, Button, TextInput, View, ScrollView, StyleSheet } from "react-native";
+import type { User } from "@/components/types";
 import { insertUser } from "../components/insertUser";
 import { getDb } from "../data/db";
 
 export default function dbTest() {
+  const [username, setUsername] = useState("");
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
   async function handleInsert() {
     try {
       await insertUser({ username: "admin2", password: "password123" });
@@ -27,10 +31,58 @@ export default function dbTest() {
     }
   }
 
+  async function getUserByUsername(username: string) {
+    try {
+      const db = await getDb();
+
+      // Query for a specific user by username
+      const rows = await db.getAllAsync<User>(
+        "SELECT * FROM users WHERE username = ?",
+        [username]
+      );
+
+      if (rows.length > 0) {
+        setSelectedUser(rows[0]); // return the first matching user
+      } else {
+        console.log("No user found"); // no user found
+      }
+    } catch (err) {
+      console.error("Error getting user by username:", err);
+    }
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Button title="Insert User" onPress={handleInsert} />
+      <Button title="Insert User" onPress={handleInsert}/>
       <Button title="Get User" onPress={getUser} />
+      <TextInput
+        style={{
+          borderWidth: 1,
+          borderColor: "gray",
+          padding: 8,
+          width: 200,
+          marginBottom: 10,
+        }}
+        placeholder="Enter username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <Button
+        title="Get User by Username"
+        onPress={() => getUserByUsername(username)}
+      />
+
+      <ScrollView style={styles.result}>
+        {selectedUser && (
+          <>
+            <Text style={styles.title}>User Found:</Text>
+            <Text>
+              {selectedUser.id}: {selectedUser.username} - {selectedUser.password}
+            </Text>
+          </>
+        )}
+      </ScrollView>
+
     </View>
   );
 }
