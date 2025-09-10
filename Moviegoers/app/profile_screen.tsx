b304c5f3cae6ce812e396eafceaa887896defaa9
Jacob Zamora <113962665+JZmor@users.noreply.github.com>
2025-09-09 19:07:30 -0700

    Added code for profile screen to be able to access the database

--- Moviegoers/app/profile_screen.tsx
+++ Moviegoers/app/profile_screen.tsx
@@ -1,16 +1,45 @@
 //Background color changed to show if working
 import { Text, View, StyleSheet } from "react-native";
-import Button from "@/components/Button"
+import type { User } from "@/components/types";
+import { useState, useEffect } from 'react';
+import Button from "@/components/Button";
+import { getDb } from "../data/db";
 
 export default function ProfileScreen() {
-    const editUsername = () => {
-       alert("text");
+    const [username, setUsername] = useState("admin2");
+    const [selectedUser, setSelectedUser] = useState<User | null>(null);
+
+    async function getUserByUsername(username: string) {
+        try {
+          const db = await getDb();
+    
+          // Query for a specific user by username
+          const rows = await db.getAllAsync<User>(
+            "SELECT * FROM users WHERE username = ?",
+            [username]
+          );
+          //console.log("worked");
+          if (rows.length > 0) {
+            setSelectedUser(rows[0]); // return the first matching user
+          } else {
+            console.log("No user found"); // no user found
+          }
+        } catch (err) {
+          console.error("Error getting user by username:", err);
+        }
     }
+    function onChangeUsername(){
+        setUsername("admin2");
+        getUserByUsername(username);
+    }
+
+    useEffect(() => {onChangeUsername()}, []);
+    
     return (
         <View style={{flex: 1,backgroundColor: "#DFC5FE"}}>
             <View style={{ alignItems: "center", marginBottom: 7}}>
-                <Text id="profileText">User's Profile</Text>
-                <Button label="Edit" onPress={editUsername}></Button>
+                <Text id="profileText">{selectedUser.username}'s Profile</Text>
+                <Button label="Edit" onPress={onChangeUsername}></Button>
             </View>
             <View style={{backgroundColor: "#FFFFFF", borderRadius: 20, alignItems: "center", alignSelf: "center", height: '20%', width: '80%', marginBottom: 7}}>
                 <Text>Favorite Movie</Text>
