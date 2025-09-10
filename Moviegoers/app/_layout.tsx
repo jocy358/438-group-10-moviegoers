7c915c6ace3530ec674873cd343392740c925735
Jacob Zamora <113962665+JZmor@users.noreply.github.com>
2025-09-10 07:28:33 -0700

    Made page for loading movie information.

--- Moviegoers/app/_layout.tsx
+++ Moviegoers/app/_layout.tsx
@@ -4,6 +4,8 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="login" options={{ title: 'Login' }} />
+      <Stack.Screen name="moviePage" options={{ title: 'Movie Page' }} />
       <Stack.Screen name="profile_screen" options={{ title: 'Profile' }} />
     </Stack>
   )
